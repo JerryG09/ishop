c0e57c428752484be79fec6fc9a0555f43bbb75d
JerryG09 <phemmy09@gmail.com>
2019-12-23 14:57:00 +0100

    passed props to productItem cmponent

--- screens/shop/ProductOverviewScreen.js
+++ screens/shop/ProductOverviewScreen.js
@@ -2,11 +2,23 @@ import React from 'react';
 import { FlatList, StyleSheet, Text } from 'react-native'
 import { useSelector } from 'react-redux';
 
+import ProductItem from '../../components/shop/ProductItem'
+
 function ProductsOverviewScreen(props) {
   const products = useSelector(state => state.products.availableProducts)
   console.log(products)
 
-return <FlatList data={products} keyExtractor={item => item.id} renderItem={itemData => <Text>{itemData.item.title}</Text>} />
+return <FlatList 
+          data={products} 
+          keyExtractor={item => item.id} 
+          renderItem={itemData => <ProductItem 
+            image={itemData.item.imageUrl}
+            title={itemData.item.title}
+            price={itemData.item.price}
+            onViewDetail={() => {}}
+            onAddToCart={() => {}}
+            />} 
+        />
 }
 
 ProductsOverviewScreen.navigationOptions = {
