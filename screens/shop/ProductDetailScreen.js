22d9b3ecbab2c70fe08a504477a56d5d637904da
JerryG09 <phemmy09@gmail.com>
2019-12-23 03:29:04 +0100

    created productDetailScreen component

---
+++ screens/shop/ProductDetailScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FlatList, StyleSheet } from 'react-native'
+
+function ProductDetailScreen() {
+  return <FlatList />
+}
+
+const styles = StyleSheet.create({
+  
+})
+
+export default ProductDetailScreen
