bd29c881e16d72835f80c4435df1d1200e5561bf
JerryG09 <phemmy09@gmail.com>
2019-12-23 04:31:43 +0100

    removed unused styles

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { enableScreens } from 'react-native-screens';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
+import ShopNavigator from './navigation/ShopNavigation'
 
 import productReducer from './store/reducers/products'
+
+enableScreens()
 
 const rootReducer = combineReducers({
   products: productReducer
@@ -14,18 +17,8 @@ const store  = createStore(rootReducer)
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <ShopNavigator />
     </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
