0b79ce096b8e9f3bf631dad87d1b3acb61f4fc67
JerryG09 <phemmy09@gmail.com>
2020-01-13 22:26:31 +0100

    fix

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import { FlatList, StyleSheet } from 'react-native'
+import React from 'react'
+import { ScrollView, View, Text, Image, Button, StyleSheet } from 'react-native';
+import { useSelector } from 'react-redux'
+// import ProductsOverviewScreen from './ProductOverviewScreen';
 
-function ProductDetailScreen() {
-  return <FlatList />
+const ProductDetailScreen = props => {
+  const productId = props.navigation.getParam('productId');
+  const selectedProduct = useSelector(state => state.products.availableProducts.find(prod => prod.id === productId))
+
+  // console.log(selectedProduct.title)
+  return(
+    <View>
+      <Text>The Product Details Screen!!!</Text>
+      <Text>{selectedProduct.title}</Text>
+      <Text>${selectedProduct.price}</Text>
+    </View>
+  )
+};
+
+ProductDetailScreen.navigationOptions = navData => {
+  // console.log(navData.navigation.getParam('productTitle'))
+  return {
+    headerTitle: navData.navigation.getParam('productTitle')
+  };
 }
 
-const styles = StyleSheet.create({
-  
-})
+const styles = StyleSheet.create({});
 
-export default ProductDetailScreen
+export default ProductDetailScreen;
