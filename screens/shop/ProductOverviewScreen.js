4628b2a1827e4d967f7702b77d796fcbec899922
JerryG09 <phemmy09@gmail.com>
2020-01-14 07:29:33 +0100

    added dispatch to cart

--- screens/shop/ProductOverviewScreen.js
+++ screens/shop/ProductOverviewScreen.js
@@ -1,27 +1,33 @@
 import React from 'react';
 import { FlatList, StyleSheet, Text } from 'react-native'
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import ProductItem from '../../components/shop/ProductItem'
+import * as cartAction from '../../store/actions/cart';
 
 function ProductsOverviewScreen(props) {
   const products = useSelector(state => state.products.availableProducts)
-  // console.log(products)
+  const dispatch = useDispatch()
 
-return <FlatList 
-          data={products} 
-          keyExtractor={item => item.id} 
-          renderItem={itemData => <ProductItem 
-            image={itemData.item.imageUrl}
-            title={itemData.item.title}
-            price={itemData.item.price}
-            onViewDetail={() => {props.navigation.navigate('ProductDetail', { 
-              productId: itemData.item.id, 
-              productTitle: itemData.item.title
-            })}}
-            onAddToCart={() => {}}
-            />} 
-        />
+return (
+  <FlatList 
+    data={products} 
+    keyExtractor={item => item.id} 
+    renderItem={itemData => (
+      <ProductItem 
+        image={itemData.item.imageUrl}
+        title={itemData.item.title}
+        price={itemData.item.price}
+        onViewDetail={() => {props.navigation.navigate('ProductDetail', { 
+          productId: itemData.item.id, 
+          productTitle: itemData.item.title
+        })}}
+        onAddToCart={() => {
+          dispatch(cartAction.addToCart(itemData.item))
+        }}
+      />
+    )}
+  />)
 }
 
 ProductsOverviewScreen.navigationOptions = {
