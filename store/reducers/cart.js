f7a57102a9fe2b54751eebbae9d67ce2375cc10f
JerryG09 <phemmy09@gmail.com>
2020-01-15 03:32:34 +0100

    removed redundant codes

--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -8,7 +8,7 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case ADD_TO_CART: 
+    case ADD_TO_CART:
       const addedProduct = action.product;
       const prodPrice = addedProduct.price;
       const prodTitle = addedProduct.title;
@@ -17,27 +17,20 @@ export default (state = initialState, action) => {
 
       if (state.items[addedProduct.id]) {
         // already have the item in the cart
-        const updatedCartItem = new CartItem(
+        updatedOrNewCartItem = new CartItem(
           state.items[addedProduct.id].quantity + 1,
           prodPrice,
           prodTitle,
           state.items[addedProduct.id].sum + prodPrice
-        )
-        return {
-          ...state,
-          items: { ...state.items, [addedProduct.id]: updatedCartItem },
-          totalAmount: state.totalAmount + prodPrice
-        }
+        );
+      } else {
+        updatedOrNewCartItem = new CartItem(1, prodPrice, prodTitle, prodPrice);
       }
-      else {
-        // item is new in the cart
-        const newCartItem = new CartItem(1, prodPrice, prodTitle, prodPrice)
-        return {
-          ...state,
-          items: { ...state.items, [addedProduct.id]: newCartItem },
-          totalAmount: state.totalAmount + prodPrice
-        }
-      }
+      return {
+        ...state,
+        items: { ...state.items, [addedProduct.id]: updatedOrNewCartItem },
+        totalAmount: state.totalAmount + prodPrice
+      };
   }
-  return state; 
-}
+  return state;
+};
