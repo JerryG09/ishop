e5d17444ad94c6c21e6e17ae2c3c0ea344dc9c58
JerryG09 <phemmy09@gmail.com>
2020-01-15 18:48:59 +0100

    added header navigation to edit screen

--- screens/user/UserProductsScreen.js
+++ screens/user/UserProductsScreen.js
@@ -67,6 +67,17 @@ UserProductScreen.navigationOptions = navData => {
         />
       </HeaderButtons>
     ),
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Add"
+          iconName={Platform.OS === 'android' ? 'md-create' : 'ios-create'}
+          onPress={() => {
+              navData.navigation.navigate('EditProduct');
+          }}
+        />
+      </HeaderButtons>
+    ),
   }
 }
 
