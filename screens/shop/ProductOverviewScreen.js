caa72d702b8a9c4396ca75615ebee4f2334cbf44
JerryG09 <phemmy09@gmail.com>
2020-01-15 03:35:48 +0100

    implemented navigation to cart screen

--- screens/shop/ProductOverviewScreen.js
+++ screens/shop/ProductOverviewScreen.js
@@ -1,41 +1,56 @@
 import React from 'react';
-import { FlatList, StyleSheet, Text } from 'react-native'
+import { FlatList, StyleSheet, Platform } from 'react-native'
 import { useSelector, useDispatch } from 'react-redux';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import ProductItem from '../../components/shop/ProductItem'
 import * as cartAction from '../../store/actions/cart';
+import HeaderButton from '../../components/UI/HeaderButton'
 
-function ProductsOverviewScreen(props) {
-  const products = useSelector(state => state.products.availableProducts)
-  const dispatch = useDispatch()
 
-return (
-  <FlatList 
-    data={products} 
-    keyExtractor={item => item.id} 
-    renderItem={itemData => (
-      <ProductItem 
-        image={itemData.item.imageUrl}
-        title={itemData.item.title}
-        price={itemData.item.price}
-        onViewDetail={() => {props.navigation.navigate('ProductDetail', { 
-          productId: itemData.item.id, 
-          productTitle: itemData.item.title
-        })}}
-        onAddToCart={() => {
-          dispatch(cartAction.addToCart(itemData.item))
-        }}
-      />
-    )}
-  />)
-}
+const ProductsOverviewScreen = props => {
+  const products = useSelector(state => state.products.availableProducts);
+  const dispatch = useDispatch();
 
-ProductsOverviewScreen.navigationOptions = {
-  headerTitle: 'All Products'
-}
+  return (
+    <FlatList
+      data={products}
+      keyExtractor={item => item.id}
+      renderItem={itemData => (
+        <ProductItem
+          image={itemData.item.imageUrl}
+          title={itemData.item.title}
+          price={itemData.item.price}
+          onViewDetail={() => {
+            props.navigation.navigate('ProductDetail', {
+              productId: itemData.item.id,
+              productTitle: itemData.item.title
+            });
+          }}
+          onAddToCart={() => {
+            dispatch(cartAction.addToCart(itemData.item));
+          }}
+        />
+      )}
+    />
+  );
+};
 
-const styles = StyleSheet.create({
-  
-})
+ProductsOverviewScreen.navigationOptions = navData => {
+  return {
+    headerTitle: 'All Products',
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Cart"
+          iconName={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'}
+          onPress={() => {
+              navData.navigation.navigate('Cart')
+          }}
+        />
+      </HeaderButtons>
+    )
+  };
+};
 
-export default ProductsOverviewScreen
+export default ProductsOverviewScreen;
