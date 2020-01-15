1ddba7bbf8f719094757ce9e167f2ba08b067acd
JerryG09 <phemmy09@gmail.com>
2020-01-15 18:08:46 +0100

    passed deleteable props

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -51,6 +51,7 @@ const CartScreen = props => {
             quantity={itemData.item.quantity}
             title={itemData.item.productTitle}
             amount={itemData.item.sum}
+            deletable
             onRemove={() => {
               dispatch(cartActions.removeFromCart(itemData.item.productId))
             }}
@@ -60,6 +61,10 @@ const CartScreen = props => {
     </View>
   );
 };
+
+CartScreen.navigationOptions = {
+  headerTitle: 'Your Cart'
+}
 
 const styles = StyleSheet.create({
   screen: {
