2e234a855dc08e07070fbf01a34ebd164d6a5924
JerryG09 <phemmy09@gmail.com>
2020-01-15 14:04:58 +0100

    WIP: toggle render order details

--- components/shop/OrderItem.js
+++ components/shop/OrderItem.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 
-import CartIem from './CartItem';
+import CartItem from './CartItem';
 import Color from '../../constants/Colors'
 
 const OrderItem = props => {
+  const [showDetails, setShowDetails] = useState(false);
+
   return (
     <View style={styles.orderItem}>
       <View style={styles.summary}>
         <Text style={styles.totalAmount}>${props.amount.toFixed(2)}</Text>
         <Text style={styles.date}>{props.date}</Text>
       </View>
-      <Button color={Color.primary} title="Show Details" />
+      <Button 
+        color={Color.primary} 
+        title={showDetails ? "Hide Details" : "Show Details" } 
+        onPress={() => {
+          setShowDetails(prevState => !prevState)
+        }}
+      />
+      
+      {showDetails && (
+        <View style={styles.detailItem}>
+          {props.items.map(cartItem => {
+            // Todo: Not displaying
+
+            
+            // {console.log(cartItem)}
+            // {console.log(showDetails)}
+            <CartItem 
+              key={cartItem.productId}
+              quantity={cartItem.quantity}
+              amount={cartItem.sum}
+              title={cartItem.productTitle}
+            />
+          })}
+        </View>
+      )}
     </View>
   )
 }
@@ -44,6 +70,9 @@ const styles = StyleSheet.create({
     fontFamily: 'open-sans',
     fontSize: 16,
     color: '#888'
+  },
+  detailItem: {
+    width: '100%'
   }
 })
 
