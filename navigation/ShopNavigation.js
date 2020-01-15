0def114559267a5e2318f0c9b930180c37dc47de
JerryG09 <phemmy09@gmail.com>
2020-01-15 08:24:18 +0100

    implemented drawer navigation

--- navigation/ShopNavigation.js
+++ navigation/ShopNavigation.js
@@ -1,32 +1,49 @@
-import {
-  createAppContainer,
-} from 'react-navigation';
+import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 import { Platform } from 'react-native'
 
 import ProductOverviewScreen from '../screens/shop/ProductOverviewScreen';
-import ProductDetailScreen from '../screens/shop/ProductDetailScreen'
+import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
 import CartScreen from '../screens/shop/CartScreen';
+import OrderScreen from '../screens/shop/OrdersScreen';
 
 import Colors from '../constants/Colors';
+
+const defaultNavOptions = {
+  headerStyle: {
+    backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
+  },
+  headerTitleStyle: {
+    fontFamily: 'open-sans-bold'
+  },
+  headerBackTitleStyle: {
+    fontFamily: 'open-sans'
+  },
+  headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
+}
 
 const ProductNavigator = createStackNavigator({
   ProductOverview: ProductOverviewScreen,
   ProductDetail: ProductDetailScreen,
   Cart: CartScreen,
 }, {
-  defaultNavigationOptions: {
-    headerStyle: {
-      backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
-    },
-    headerTitleStyle: {
-      fontFamily: 'open-sans-bold'
-    },
-    headerBackTitleStyle: {
-      fontFamily: 'open-sans'
-    },
-    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
+  defaultNavigationOptions: defaultNavOptions
+});
+
+const OrdersNavigator = createStackNavigator({
+  Orders: OrderScreen
+}, {
+  defaultNavigationOptions: defaultNavOptions
+})
+
+const ShopNavigator = createDrawerNavigator({
+  Products: ProductNavigator,
+  Orders: OrdersNavigator
+}, {
+  contentOptions: {
+    activeTintColor: Colors.primary
   }
 })
 
-export default createAppContainer(ProductNavigator)
+export default createAppContainer(ShopNavigator)
