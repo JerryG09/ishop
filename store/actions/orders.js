0fae3cc06414785f11635d9c7be533138f2556a3
JerryG09 <phemmy09@gmail.com>
2020-01-19 00:04:31 +0100

    added fetch orders action creator

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,4 +1,41 @@
+import Order from "../../models/order";
+
 export const ADD_ORDER = "ADD_ORDER";
+export default SET_ORDERS = 'SET_ORDERS'
+
+export const fetchOrders = () => {
+  return async dispatch => {
+    try {
+      const response = await fetch('https://ishop-2c8bd.firebaseio.com/orders/u1.json');
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!')
+      }
+  
+      const resData = await response.json()
+      const loadedOrders = [];
+  
+      for (const key in resData) {
+        loadedOrders.push(
+          new Order(
+            key,
+            resData[key].cartItems,
+            resData[key].totalAmount,
+            new Date(resData[key].date)
+          )
+        )
+      }
+  
+      dispatch({
+        type: SET_ORDERS,
+        orders: loadedOrders
+      })
+    } catch (error) {
+      // TODO send to custom analytic server
+      throw error
+    }
+  }
+}
 
 export const addOrder = (cartItems, totalAmount) => {
   return async dispatch => {
