9d770e566d7a0013ac43116a37e7c529f0eaa6fc
JerryG09 <phemmy09@gmail.com>
2020-01-20 06:55:18 +0100

    added login action creator

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -1,4 +1,5 @@
 export const SIGNUP = 'SIGNUP';
+export const LOGIN = 'LOGIN'
 
 export const signup = (email, password) => {
   return async dispatch => {
@@ -24,6 +25,34 @@ export const signup = (email, password) => {
 
     dispatch({
       type: SIGNUP,
+    })
+  }
+}
+
+export const login = (email, password) => {
+  return async dispatch => {
+    const response = fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCCtCJhw0co50LLLFBV_OL-2gsc8aJcezY', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email,
+        password,
+        returnSecureToken: true
+      })
+    });
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
+    }
+
+    const resData = await (await response).json()
+
+    console.log(resData)
+
+    dispatch({
+      type: LOGIN,
     })
   }
 }
