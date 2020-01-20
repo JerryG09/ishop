7427613022e6d21f22701a0558c7000ebfe578d7
JerryG09 <phemmy09@gmail.com>
2020-01-20 05:16:00 +0100

    added signup action creator

---
+++ store/actions/auth.js
@@ -0,0 +1,29 @@
+export const SIGNUP = 'SIGNUP';
+
+export const signup = (email, password) => {
+  return async dispatch => {
+    const response = fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCCtCJhw0co50LLLFBV_OL-2gsc8aJcezY', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email,
+        password,
+        returnSecureToken: true
+      })
+    });
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
+    }
+
+    const resData = await (await response).json()
+    
+    console.log(resData)
+
+    dispatch({
+      type: SIGNUP,
+    })
+  }
+}
